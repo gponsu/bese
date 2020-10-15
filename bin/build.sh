eda2bd93e37739ce3cca353701e64f341ba0af6d
gponsu <simpons@gmail.com>
2020-10-15 14:37:00 +0200

    chore: introduce docker support to storage service

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+docker network create --driver bridge skynet
 docker volume create bese_database
+
 docker build -t bese:2.0.0 ./app
+docker build -t storage:1.0.0 ./storage
--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-docker run -it \
-  --publish 8080:4567 \
+docker run -itd --rm \
+  --name storage \
+  --network skynet \
   --mount source=bese_database,target=/usr/local/databases \
+  storage:1.0.0
+
+docker run -it --rm \
+  --network skynet \
+  --publish 8080:4567 \
   --env-file ./app/.env \
   bese:2.0.0
