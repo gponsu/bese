ada9330328478e95fdc920d9eb195f23d980cf27
gponsu <simpons@gmail.com>
2020-10-15 12:47:49 +0200

    fix: the api of cryptos stopped working, using a new one now

--- services/bitcoin_quote.rb
+++ services/bitcoin_quote.rb
@@ -7,8 +7,8 @@ module BitcoinQuote
 
   def retrieve
     quote = find_quote
-    price = quote['price_usd'].to_f
-    change = quote['percent_change_24h'].to_f
+    price = quote[:price_usd]
+    change = quote[:percent_change_24h]
 
     Bitcoin::Entity.new(price, change)
   end
@@ -16,8 +16,18 @@ module BitcoinQuote
   private
 
   def find_quote
-    url = URI('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
+    url = URI('https://api.pro.coinbase.com/products/BTC-USD/stats')
     response = Net::HTTP.get(url)
-    JSON.parse(response).first
+    parse(response)
+  end
+
+  def parse(response)
+    parsed = JSON.parse(response, symbolize_names: true)
+    percent_change_24h = ((parsed[:last].to_f - parsed[:open].to_f)/parsed[:open].to_f*100).round(2)
+
+    {
+      price_usd: parsed[:last].to_f,
+      percent_change_24h: percent_change_24h
+    }
   end
 end
